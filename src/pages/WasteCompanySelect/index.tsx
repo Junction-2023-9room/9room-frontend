3437aec536c2fb88fde1d7df4fca71515ab2d0d3
sumi-0011 <selina2000@naver.com>
2023-08-19 18:56:25 +0900

    feat: recommend company select view create

--- src/pages/WasteCompanySelect/index.tsx
+++ src/pages/WasteCompanySelect/index.tsx
@@ -1,13 +1,36 @@
+import { useLocation } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 import TitleHeader from '../../components/Header/TitleHeader';
-import { Company, COMPANY_LIST } from '../../constants/company';
+import { COMPANY_LIST } from '../../constants/company';
+import CompanyItem from './CompanyItem';
 
 const Index = () => {
+  const location = useLocation();
+
+  const params = new URLSearchParams(location.search);
+  const wasteName = params.get('waste');
+
   return (
     <div>
       <TitleHeader />
+      <TopContainer>
+        <Heading>추천 업체</Heading>
+        <RecommendCompany>
+          <span>{wasteName}</span>
 
+          <img src="/icons/drop-arrow.svg" alt="drop-arrow" />
+        </RecommendCompany>
+
+        <TotalSection>
+          <span>Total {COMPANY_LIST.length}</span>
+          <div>
+            <span>Default</span>
+            <img src="/icons/down-arrow.svg" alt="down-arrow" />
+          </div>
+        </TotalSection>
+      </TopContainer>
+      <Divider />
       <List>
         {COMPANY_LIST.map((company) => (
           <CompanyItem key={company.name} {...company} />
@@ -23,136 +46,64 @@ const List = styled.div`
   padding: 21px 20px;
 `;
 
-const CompanyItem = (company: Company) => {
-  return (
-    <ItemContainer>
-      <ItemImageContainer>
-        <img src={company.img} alt={company.name} />
-        <div>{company.cheaper} cheaper</div>
-      </ItemImageContainer>
-      <div>
-        <ItemNameContainer>
-          <img src={company.nameImg} alt={company.name} />
-        </ItemNameContainer>
-        <CompanyInfoContainer>
-          <div>
-            <img src="/icons/star-1.svg" alt="star" />
-            <span>{company.rating}</span>
-          </div>
-          <div>
-            <img src="/icons/location.svg" alt="location" />
-            <span>{company.location}</span>
-          </div>
-          <div>
-            <img src="/icons/time.svg" alt="time" />
-            <span>{company.time}</span>
-          </div>
-        </CompanyInfoContainer>
-        <ItemDesc>
-          <div>{company.desc}</div>
-        </ItemDesc>
-      </div>
-    </ItemContainer>
-  );
-};
-
-const ItemContainer = styled.div`
-  display: grid;
-  height: 108px;
-  grid-template-columns: 90px 1fr;
-  gap: 12px;
-  margin-bottom: 20.88px;
-
-  & > div {
-    display: flex;
-    flex-direction: column;
-    gap: 12px;
-    justify-content: space-between;
-  }
+const TopContainer = styled.div`
+  padding: 0 20px;
 `;
 
-const ItemImageContainer = styled.div`
-  width: 90px;
-  position: relative;
-  overflow: hidden;
-  border-radius: 14px;
-  background: #444;
+const TotalSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 52px;
+  align-items: center;
 
-  & > div {
-    position: absolute;
-    width: 100%;
-    bottom: 0;
-
-    background: #4c35ff;
-    height: 26.842px;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    color: #fff;
-    font-size: 10px;
-    font-weight: 700;
-  }
-`;
-
-const ItemNameContainer = styled.div`
-  height: 23px;
-`;
-
-const CompanyInfoContainer = styled.section`
-  grid-template-columns: 1fr 1.5fr 1.5fr;
-  display: grid;
-
-  & > div {
-    display: flex;
-    color: #666;
-    font-size: 12px;
-    font-weight: 400;
-    gap: 4px;
-    align-items: center;
-
-    &:nth-child(1) {
-      color: #313131;
-      font-size: 14px;
-      font-weight: 700;
-      img {
-        height: 12px;
-      }
-    }
-
-    &:nth-child(2) {
-      img {
-        height: 10px;
-      }
-    }
-
-    &:nth-child(3) {
-      img {
-        height: 10px;
-      }
-    }
-  }
-`;
-
-const ItemDesc = styled.section`
-  border-radius: 4px;
-  background: #efefef;
-  padding: 10px 8px;
-
-  color: #a1a1a1;
-  font-size: 12px;
-  font-style: normal;
+  color: #666;
+  font-size: 14px;
   font-weight: 400;
-  line-height: 87%; /* 10.44px */
+
+  img {
+    width: 12px;
+    height: 7px;
+    position: relative;
+    top: 1px;
+  }
+
+  & > div {
+    font-weight: 500;
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    gap: 8px;
+  }
+`;
+
+const Heading = styled.h1`
+  margin-top: 24px;
+  margin-bottom: 20px;
+  color: #323232;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 100%; /* 20px */
+`;
+
+const RecommendCompany = styled.div`
+  border-radius: 9px;
+  border: 1px solid #c1c1c1;
+  background: #fff;
+  height: 52px;
+  padding: 0 18px;
+  line-height: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Divider = styled.hr`
+  background: #f4f4f4;
+  height: 12px;
+  border: none;
 
   width: 100%;
-
-  & > div {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-  }
 `;
