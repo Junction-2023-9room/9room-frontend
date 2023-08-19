ea7731dd6cd2ab8f171e64b10a27d039bf842508
sumi-0011 <selina2000@naver.com>
2023-08-19 12:19:30 +0900

    style: layout component style add

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,20 @@
-function Layout() {
-  return <div>Layout</div>;
+import { PropsWithChildren } from 'react';
+import { styled } from 'styled-components';
+
+import theme from '../styles/theme';
+
+interface Props {
+  title?: string;
+}
+
+function Layout({ children }: PropsWithChildren<Props>) {
+  return <Container>{children}</Container>;
 }
 
 export default Layout;
+
+const Container = styled.main`
+  max-width: ${theme.maxWidth};
+  min-height: 100vh;
+  margin: 0 auto;
+`;
