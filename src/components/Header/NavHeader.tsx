462fec86fb14a7a8295674c22a41d7967045b1eb
bbung <tjd123123@naver.com>
2023-08-19 14:56:07 +0900

    feat: header component create

---
+++ src/components/Header/NavHeader.tsx
@@ -0,0 +1,60 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+interface Props {
+  title: string;
+}
+
+const NavHeader = ({ title }: Props) => {
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate(-1);
+  };
+
+  return (
+    <NavHeaderContainer>
+      <button onClick={handleOnClick}>
+        <img src={'/icons/back-arrow.svg'} alt="backk-arrow" />
+      </button>
+      <h1>{title}</h1>
+    </NavHeaderContainer>
+  );
+};
+
+export default NavHeader;
+
+const NavHeaderContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+
+  & > button {
+    cursor: pointer;
+    position: absolute;
+    left: 20px;
+    top: 50%;
+    height: 100%;
+    background: none;
+    border: none;
+    transform: translateY(-50%);
+
+    & > img {
+      display: flex;
+      align-items: center;
+      width: 18px;
+      height: 18px;
+    }
+  }
+
+  & > h1 {
+    color: #222;
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 100%;
+  }
+`;
