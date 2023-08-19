1d21437301490358ba28a59bd297d3b9b360469f
bbung <tjd123123@naver.com>
2023-08-20 03:41:38 +0900

    chore: waste-select heading fix

--- src/pages/Waste/index.tsx
+++ src/pages/Waste/index.tsx
@@ -8,7 +8,10 @@ const Index = () => {
     <div>
       <TitleHeader />
       <Container>
-        <Heading>Choose your waste!</Heading>
+        <Heading>
+          Choose your <br />
+          <strong>Generous</strong> waste!
+        </Heading>
         <Desc>Ensure a safe and responsible disposal</Desc>
         <WasteList />
       </Container>
@@ -29,6 +32,10 @@ const Heading = styled.h2`
   color: #101010;
   font-size: 28px;
   font-weight: 700;
+
+  & > strong {
+    color: #4c35ff;
+  }
 `;
 
 const Desc = styled.p`
