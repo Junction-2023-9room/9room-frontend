c267d792a6b9c78d47b93792d9a0244a3350d94d
sumi-0011 <selina2000@naver.com>
2023-08-19 16:38:42 +0900

    feat: company info list up

---
+++ src/constants/company.ts
@@ -0,0 +1,43 @@
+export interface Company {
+  name: string;
+  nameImg: string;
+  img: string;
+  location: string;
+  time: string;
+  rating: string;
+  desc: string;
+  cheaper: string;
+}
+
+export const COMPANY_LIST: Company[] = [
+  {
+    name: 'Jaka',
+    nameImg: '/images/company/jaka-logo.svg',
+    img: '/images/company/jaka-img.png',
+    location: 'Near Seoul',
+    time: 'Within 2 days',
+    rating: '4.5',
+    desc: 'Impressed with their use of cobot. The disposal process was not only efficiThe disposal process was not only effici',
+    cheaper: '30%',
+  },
+  {
+    name: 'Jaka2',
+    nameImg: '/images/company/jaka-logo.svg',
+    img: '/images/company/jaka-img.png',
+    location: 'Near Seoul',
+    time: 'Within 2 days',
+    rating: '4.5',
+    desc: 'Impressed with their use of cobot. The disposal process was not only effici',
+    cheaper: '30%',
+  },
+  {
+    name: 'Jaka3',
+    nameImg: '/images/company/jaka-logo.svg',
+    img: '/images/company/jaka-img.png',
+    location: 'Near Seoul',
+    time: 'Within 2 days',
+    rating: '4.5',
+    desc: 'Impressed with their use of cobot. The disposal process was not only effici',
+    cheaper: '30%',
+  },
+];
