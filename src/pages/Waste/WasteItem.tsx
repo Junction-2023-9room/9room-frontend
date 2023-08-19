f63cf4726ef7c38c322d8ba53aab3c973c88915b
sumi-0011 <selina2000@naver.com>
2023-08-19 14:11:57 +0900

    add: waste item component

---
+++ src/pages/Waste/WasteItem.tsx
@@ -0,0 +1,109 @@
+import styled from 'styled-components';
+
+import {
+  CheckIcon,
+  SmallCheckIcon,
+  SmallNonCheckIcon,
+} from '../../assets/icons/check-icon';
+
+interface ItemProps {
+  label: string;
+  id: string;
+}
+
+interface Props {
+  label: string;
+  id: string;
+  onClick: (id: string) => void;
+  selectItem: string | null;
+
+  subItems?: ItemProps[];
+}
+
+const WasteItem = ({ label, id, onClick, selectItem, subItems }: Props) => {
+  const isChecked = Boolean(
+    selectItem === id || (selectItem && subItems && checkSubItem(selectItem, subItems)),
+  );
+  return (
+    <div>
+      <Item onClick={() => onClick(id)} isChecked={isChecked}>
+        <div>
+          <img src="/images/pesticides.svg" alt="pesticides" />
+          <span>{label}</span>
+        </div>
+        <CheckIcon fill={isChecked ? '#FFF' : '#BABABA'} />
+      </Item>
+
+      {isChecked && (
+        <SubItemContainer>
+          {subItems?.map((item) => (
+            <SubItem
+              isChecked={item.id === selectItem}
+              key={item.id}
+              onClick={() => onClick(item.id)}
+            >
+              <div>
+                <img src="/images/pesticides.svg" alt="pesticides" />
+                <span>{item.label}</span>
+              </div>
+              {item.id === selectItem ? <SmallCheckIcon /> : <SmallNonCheckIcon />}
+            </SubItem>
+          ))}
+        </SubItemContainer>
+      )}
+    </div>
+  );
+};
+export default WasteItem;
+
+const Item = styled.div<{
+  isChecked: boolean;
+}>`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 14px;
+  height: 63px;
+  align-items: center;
+  border-radius: 12px;
+
+  font-weight: 700;
+  color: ${(props) => (props.isChecked ? '#fff' : '#595959')};
+
+  background-color: ${(props) => (props.isChecked ? '#4C35FF' : '#FFF')};
+  border: 1px solid;
+  border-color: ${(props) => (props.isChecked ? '#4C35FF' : '#C1C1C1')};
+
+  & > div {
+    align-items: center;
+    display: flex;
+    gap: 14px;
+  }
+`;
+
+const SubItemContainer = styled.div`
+  border-radius: 12px;
+  background: #efefef;
+  margin-top: 4px;
+  padding: 8px;
+`;
+
+const SubItem = styled(Item)<{
+  isChecked: boolean;
+}>`
+  background-color: ${(props) => (props.isChecked ? '#D9D9D9' : 'transparent')};
+  padding: 6px 12px;
+  height: 48px;
+  color: #595959;
+  border: none;
+`;
+
+const checkSubItem = (selectItem: string, subItem: ItemProps[]) => {
+  let flag = false;
+  subItem.forEach((item) => {
+    if (item.id === selectItem) {
+      flag = true;
+    }
+  });
+
+  return flag;
+};
--- src/pages/Waste/index.tsx
+++ src/pages/Waste/index.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { styled } from 'styled-components';
+
+import WasteItem from './WasteItem';
 
 const Index = () => {
-  return <div>Waste</div>;
+  const [selectItem, setSelectItem] = useState<string | null>(null);
+
+  const onItemClick = (id: string) => {
+    if (id === selectItem) {
+      setSelectItem(null);
+      return;
+    }
+    setSelectItem(id);
+  };
+
+  return (
+    <div>
+      <ItemList>
+        <WasteItem
+          label={'Pesticides'}
+          id={'1'}
+          onClick={onItemClick}
+          selectItem={selectItem}
+        />
+        <WasteItem
+          label={'Pesticides'}
+          id={'11'}
+          onClick={onItemClick}
+          selectItem={selectItem}
+          subItems={[
+            {
+              label: 'aa',
+              id: '2',
+            },
+            {
+              label: '3333',
+              id: '4',
+            },
+          ]}
+        />
+        <WasteItem
+          label={'Pesticides'}
+          id={'41'}
+          onClick={onItemClick}
+          selectItem={selectItem}
+        />
+      </ItemList>
+    </div>
+  );
 };
 
 export default Index;
+
+const ItemList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
