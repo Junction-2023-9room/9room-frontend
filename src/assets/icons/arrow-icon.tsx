087d90d36b60f2b9ef8430ce2687e57351e8f298
sumi-0011 <selina2000@naver.com>
2023-08-20 07:55:16 +0900

    feat: drop box add

---
+++ src/assets/icons/arrow-icon.tsx
@@ -0,0 +1,33 @@
+export function DownArrowIcon() {
+  return (
+    <svg
+      width="16"
+      height="9"
+      viewBox="0 0 16 9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8 9L0 1.36749L1.43333 0L8 6.29682L14.5667 0.0318021L16 1.39929L8 9Z"
+        fill="#6D6D6D"
+      />
+    </svg>
+  );
+}
+
+export function UpArrowIcon() {
+  return (
+    <svg
+      width="16"
+      height="9"
+      viewBox="0 0 16 9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8 -6.99382e-07L16 7.63251L14.5667 9L8 2.70318L1.43333 8.9682L1.2233e-07 7.60071L8 -6.99382e-07Z"
+        fill="#6D6D6D"
+      />
+    </svg>
+  );
+}
--- src/pages/Process/TrackOrderList.tsx
+++ src/pages/Process/TrackOrderList.tsx
@@ -9,12 +9,26 @@ const OrderData = [
     src: '/images/process/booked-done.svg',
     activeSrc: '/images/process/booked.svg',
     date: new Date(),
+    detail: {
+      status: 'Booked',
+      desc: `The waste has been filed with our company now!
+      Our dedicated team will dispose of it quickly. Don't worry, you're already
+       The right choice!`,
+    },
   },
   {
     title: 'In Transit',
     src: '/images/process/in-transit-done.svg',
     activeSrc: '/images/process/in-transit.svg',
     date: new Date(),
+
+    detail: {
+      status: 'On the Move',
+      desc: `Your waste is currently on its way to our 
+      facility. Our dedicated team ensures a swift
+       and safe transit. Rest assured, you've made
+       the right choice!`,
+    },
   },
   {
     title: 'Awaiting Processing',
@@ -30,6 +44,7 @@ const OrderData = [
   },
 ];
 
+// Current Status
 interface Props {
   stage: number;
 }
